1f66dd4db771ea84a6a0188e7081a2871487714e
Jake Rundle <jrundle1618@gmail.com>
2024-10-09 18:59:18 -0400

    Groups
    Added groups for orc troops and enemy troops

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -3,23 +3,23 @@ extends StaticBody3D
 var bullet
 var damage : int = 5
 var targets : Array = []
-var cur_tar : CharacterBody3D
+var cur_tar : Unit
 var can_shoot : bool = true
 
 func choose_target(targets : Array) -> void:
 	var temp_array : Array = targets
-	var current_target : CharacterBody3D = null
+	var current_target : Unit = null
 	for i in temp_array:
 		if current_target == null:
 			current_target = i
 	cur_tar = current_target
 
 func _on_detection_range_body_entered(body):
-	if body.is_in_group("Enemy"):
+	if body.is_in_group("Orc Troop"):
 		targets.append(body)
 		choose_target(targets)
 
 func _on_detection_range_body_exited(body):
-	if body.is_in_group("Enemy"):
+	if body.is_in_group("Orc Troop"):
 		targets.erase(body)
 		choose_target(targets)
