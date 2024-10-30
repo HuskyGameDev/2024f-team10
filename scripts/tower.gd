59702671c58758be4287f9d62e001fae39415d21
Jake Rundle <jrundle1618@gmail.com>
2024-10-30 17:18:16 -0400

    Tower Group Fix

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -25,7 +25,7 @@ func _process(delta):
 	if is_instance_valid(cur_tar):
 		look_at(cur_tar.global_position)
 		if can_shoot:
-			#shoot()
+			shoot()
 			can_shoot = false
 			$FireRate.start()
 	else:
