db646f726ca7987f3adf20e27a1d86fc68000964
Jake Rundle <jrundle1618@gmail.com>
2024-10-30 18:55:31 -0400

    Update resource_tower.gd

--- scripts/interface_scripts/resource_tower.gd
+++ scripts/interface_scripts/resource_tower.gd
@@ -1,11 +1,10 @@
 extends StaticBody3D
 
 var health : int = 100
-
 func take_damage(dmg : float):
 	health -= dmg
 	if health <= 0:
-		
+		emeraldCount += 15
 		queue_free()
 
 func _process(delta):
