3b46e8e7fc000c8f486455dc1b8f0e91045dd9dc
Jake Rundle <jrundle1618@gmail.com>
2024-11-06 17:47:14 -0500

    Boss Orc
    Created a boss orc implemented as a tower that is not recognized by friendly troops and slowly attacks enemy troops.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -13,6 +13,6 @@ func _physics_process(delta):
 		queue_free()
 
 func _on_collision_body_entered(body):
-	if body.is_in_group("Orc Troop"):
+	if body.is_in_group("Tower Troop"):
 		body.damage(damage)
 		queue_free()
