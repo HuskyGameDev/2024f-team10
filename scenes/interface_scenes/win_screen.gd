28c9cdbee47cca8244091072236820af52aa5e3c
Jake Rundle <jrundle1618@gmail.com>
2024-11-20 22:39:14 -0500

    Win/lose screen fixes

---
+++ scenes/interface_scenes/win_screen.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/interface_scenes/title_screen.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
