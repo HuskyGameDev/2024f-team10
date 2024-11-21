6dc6d19570aa36a798f05e25722ae422ce0c767b
Jake Rundle <jrundle1618@gmail.com>
2024-11-20 20:19:57 -0500

    changed main scene to title screen and updated title screen start button

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/tutorial.tscn")
 
 
 func _on_quit_pressed():
